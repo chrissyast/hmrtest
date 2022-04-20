7637a56c815467167d276ab7f3e08e2d1222875f
Chris <cjastles@gmail.com>
2022-04-20 22:00:11 +0100

    Initial commit

---
+++ components/MyComponent.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+  Component dsgsd
+</div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    console.log("in component")
+    console.log(this.$nuxt.context.isHMR)
+  }
+  // make some changes fdhdfh
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <my-component />
+</template>
+
+<script>
+export default {
+  components: {
+    MyComponent: () => import("~/components/MyComponent.vue")
+  },
+  mounted() {
+    console.log("in page")
+    console.log(this.$nuxt.context.isHMR)
+  }
+  // make some changes
+}
+</script>
